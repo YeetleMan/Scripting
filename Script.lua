c1f9f1b28557efa4bd9086cd726f7422ad5ad516
lIIIIIIIl <65367932+lIIIIIIIl@users.noreply.github.com>
2020-05-14 17:21:55 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,4 @@
-local model = game:GetService("InsertService"):LoadAsset(28224824)
+local model = game:GetService("InsertService"):LoadAsset(46676275)
 model.Parent = workspace	
 for i,v in pairs (model:GetDescendants()) do	
 if v:IsA("Part") then	
