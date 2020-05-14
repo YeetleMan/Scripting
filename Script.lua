ea1dabaa892665afc10d083a1e4a550847d0eb77
YeetleMan <65367932+YeetleMan@users.noreply.github.com>
2020-05-14 16:17:55 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -3,4 +3,4 @@ model.Parent = workspace
 for i,v in pairs (model:GetDescendants()) do	
 if v:IsA("Part") then	
 model.PrimaryPart = v	end	end
-model:SetPrimaryPartCFrame(game.Workspace.Player.Head.CFrame)
+model:SetPrimaryPartCFrame(game.Workspace.Player.Head.CFrame
